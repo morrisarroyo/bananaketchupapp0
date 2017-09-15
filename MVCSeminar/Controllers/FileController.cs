b8660c34f98792dd6543aba53e57b3ed79d25757
Morris Arroyo <arroyomorris@outlook.com>
2017-09-14 21:28:49 -0700

    added Files Page and access to TextFiles
    Signed-off-by: Morris Arroyo <arroyomorris@outlook.com>

---
+++ MVCSeminar/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSeminar.Controllers
+{
+    public class FileController : Controller
+    {
+
+
+        // GET: File
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            //Console.WriteLine(files[0].)           
+            for(int i = 0; i < files.Length; ++i)
+            {
+                files[i] = Path.GetFileName(files[i]);
+            }
+            return View(files);
+        }
+
+        public ActionResult Content(string file)
+        {
+            string text = System.IO.File.ReadAllText(Server.MapPath(@"~/TextFiles/") + file);
+            return View("Content", (object)text);
+        }
+    }    
+}
